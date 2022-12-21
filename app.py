f1355c9ec36c07ee79430ac4696e184ef116c56f
Mateo <matthew1438@utp.edu.co>
2022-12-21 20:25:22 +0100

    Reconstrucción del proyecto platzi

--- app.py
+++ app.py
@@ -2,36 +2,44 @@ import pickle
 import json
 from flask import Flask, request
 import pandas as pd
+import numpy as np
 
 
-FEATURES = pickle.load(open("churn/models/features.pk", "rb"))
-
-model = pickle.load(open("churn/models/model.pk", "rb"))
-column_equivalence = pickle.load(open("churn/models/column_equivalence.pk", "rb"))
+model = pickle.load(open("laboratorio-machine-learning/models/version2.0/model.pk", "rb"))
+scaler = pickle.load(open("laboratorio-machine-learning/models/version2.0/scaler.pk", "rb"))
 
 # create the Flask app
 app = Flask(__name__)
 
+def country_to_numeric(country):
+    countries={'France': [1, 0, 0], 'Germany': [0, 1, 0], 'Spain': [0, 0, 1]}
+    return countries[country]
+
 def convert_numerical(features):
     output = []
+    geography = []
+
     for i, feat in enumerate(features):
-        if i in column_equivalence:
-            output.append(column_equivalence[i][feat])
+        if i == 1:
+            geography=country_to_numeric(feat)
         else:
             try:
                 output.append(pd.to_numeric(feat))
             except:
-                output.append(0)
+                gender_to_numeric = lambda feat: 1 if feat=='Female' else 0
+                output.append(gender_to_numeric(feat))
+    output+=geography
     return output
 
 @app.route('/query')
 def query_example():
     features = convert_numerical(request.args.get('feats').split(','))
     response = {
-        'response': [int(x) for x in model.predict([features])]
+        'response': [int(x) for x in model.predict([scaler.transform(np.array(features).reshape(1, -1))[0]])]
     }
     return json.dumps(response)
 
 if __name__ == '__main__':
     # run app in debug mode on port 3001
     app.run(debug=True, port=3001)
+
